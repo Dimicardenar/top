0aba0cdeb17daf9c0cf1a0055a80b647770dcb67
Dimicardenar <mbangdimitri01@gmail.com>
2021-08-27 05:44:35 +0200

    essagedebut programme

---
+++ test.py
@@ -0,0 +1,9 @@
+def somme ():
+    print ("bonjour ou bonsoir se programme permet de calculer la somme entre deux nombres!!!")
+    a=input("entrer le premier nombre: ")
+    a=float(a)
+    b=input("entrer le deuxieme nombre: ")
+    b=float(b)
+    s=a+b
+    print("la somme entre {} et {} donne {} ".format(a,b,s))
+somme()
